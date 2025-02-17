1210be5e18c03496f5634bd1b6de14deb95bab5a
AlvaroJoaquinESP <alvaroalbarracin02@gmail.com>
2025-02-17 22:43:34 +0100

    min 51

--- CLIENTE/react/reducer/tareasJuan/src/App.jsx
+++ CLIENTE/react/reducer/tareasJuan/src/App.jsx
@@ -1,19 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import { useReducer } from "react"
 import ListarTareas from "./components/ListarTareas"
+import AddTarea from "./components/AddTarea"
 
 // datos inciales
 const initialState={
-    tareas: [],
+    tareas: [{id:1, tarea:"Limpiar la casa", limite: "mañana", prioridad:"baja"}, {id:2, tarea:"Ensuciar la casa", limite: "pasado", prioridad:"alta"}],
 }
 
 function reducer(state,action){
 switch(action.type){
     
-case '':
-    return {
-
-    }
+case 'eliminar':
+    return {tareas: state.tareas.filter(p => p.id != action.payload)}
 
     default:
         return state
@@ -28,6 +27,7 @@ const [state,dispatch]=useReducer(reducer,initialState)
 return (
 <Routes>
     <Route path="/" element={<ListarTareas state={state} dispatch={dispatch}/>}/>
+    <Route path="/nuevo" element={<AddTarea state={state} dispatch={dispatch}/>}/>
 </Routes>
 
 );
--- CLIENTE/react/reducer/tareasJuan/src/components/ListarTareas.jsx
+++ CLIENTE/react/reducer/tareasJuan/src/components/ListarTareas.jsx
@@ -1,7 +1,26 @@
-const ListasTareas = () => {
-  return (
-    <div>ListasTareas</div>
-  )
-}
+import { Link } from "react-router-dom";
 
-export default ListasTareas
+const ListasTareas = ({state, dispatch}) => {
+  return (
+    <div className="container">
+      <div className="row gap-2">
+
+        {state.tareas.map((elemento) => {
+          return (
+            <div className="col-4 border p-2" key={elemento.id}>
+              <h1>{elemento.id}</h1>
+              <h2>{elemento.tarea}</h2>
+              <h3>{elemento.limite}</h3>
+              <h4>{elemento.prioridad}</h4>
+              <button className="btn btn-danger m-1" onClick={() => dispatch({type:"eliminar", payload: elemento.id})}>Eliminar</button>
+            </div>
+          );
+        })}
+      </div>
+
+      <Link to="/nuevo" className="btn btn-success mt-3">Añadir</Link>
+    </div>
+  );
+};
+
+export default ListasTareas;
