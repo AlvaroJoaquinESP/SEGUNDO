175bfb2e1230d32ca08df518c8f27d76ba84f594
AlvaroJoaquinESP <alvaroalbarracin02@gmail.com>
2025-04-07 12:32:48 +0200

    angularclass4

---
+++ ANGULAR/2Examples/angular/app2/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PepeComponent } from './components/pepe/pepe.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PepeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ANGULAR/2Examples/angular/app2/src/app/components/pepe/pepe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pepe',
+  standalone: false,
+  templateUrl: './pepe.component.html',
+  styleUrl: './pepe.component.css'
+})
+export class PepeComponent {
+
+}
