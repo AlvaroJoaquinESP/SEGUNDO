de176455bf4e98cc6669bac2601cc43e9159b1b7
AlvaroJoaquinESP <alvaroalbarracin02@gmail.com>
2025-06-03 14:16:03 +0200

    ver batch

--- SPRING/repaso/src/main/java/com/alvaro1/repaso/repository/IClientRepository.java
+++ SPRING/repaso/src/main/java/com/alvaro1/repaso/repository/IClientRepository.java
@@ -2,6 +2,8 @@ package com.alvaro1.repaso.repository;
 
 import com.alvaro1.repaso.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IClientRepository extends JpaRepository<Client, Long> {
 }
--- SPRING/repaso/src/main/java/com/alvaro1/repaso/repository/ITeamRepository.java
+++ SPRING/repaso/src/main/java/com/alvaro1/repaso/repository/ITeamRepository.java
@@ -2,6 +2,8 @@ package com.alvaro1.repaso.repository;
 
 import com.alvaro1.repaso.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ITeamRepository extends JpaRepository<Team, Long> {
 }
