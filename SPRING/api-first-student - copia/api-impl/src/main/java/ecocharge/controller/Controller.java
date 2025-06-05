622d0d84635332d9b4107719eb83d7f48a6893a6
AlvaroJoaquinESP <alvaroalbarracin02@gmail.com>
2025-06-05 10:35:03 +0200

    jira + rename en pom de api-impl(description + name)

--- SPRING/api-first-student - copia/api-impl/src/main/java/ecocharge/controller/Controller.java
+++ SPRING/api-first-student - copia/api-impl/src/main/java/ecocharge/controller/Controller.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping({"/chargers/", "/availability/", "/station/"})
 
 public class Controller {
-
     private final IService service;
 
     public Controller(IService service) {
